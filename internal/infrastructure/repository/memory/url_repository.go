61572a081b2762e034824d15feb85db7e7af4695
maxwellgithinji <maxwellgithinji@gmail.com>
2024-08-11 09:23:51 -0400

    update in-memory database to handle concurrent executon

--- internal/infrastructure/repository/memory/url_repository.go
+++ internal/infrastructure/repository/memory/url_repository.go
@@ -1,46 +1,83 @@
 package memory
 
-import "github.com/golanglowell/quick-link/internal/domain"
+import (
+	"errors"
+
+	"github.com/golanglowell/quick-link/internal/domain"
+)
 
 type URLRepository struct {
-	urls map[string]domain.URL
+	urls     map[string]*domain.URL
+	commands chan command
+}
+
+type command interface {
+	execute(map[string]*domain.URL)
+}
+
+type saveCommand struct {
+	url    *domain.URL
+	result chan<- error
+}
+
+type findCommand struct {
+	shortCode string
+	result    chan<- findResult
+}
+
+type findResult struct {
+	url *domain.URL
+	err error
+}
+
+func (c saveCommand) execute(urls map[string]*domain.URL) {
+	urls[c.url.ShortCode] = c.url
+	c.result <- nil
+}
+
+func (c findCommand) execute(urls map[string]*domain.URL) {
+	url, ok := urls[c.shortCode]
+	if !ok {
+		c.result <- findResult{nil, errors.New("URL not found")}
+	} else {
+		c.result <- findResult{url, nil}
+	}
 }
 
 func NewURLRepository() *URLRepository {
-	return &URLRepository{
-		urls: make(map[string]domain.URL),
+
+	repo := &URLRepository{
+		urls:     make(map[string]*domain.URL),
+		commands: make(chan command),
+	}
+
+	go repo.commandLoop()
+	return repo
+}
+
+func (u *URLRepository) commandLoop() {
+	for cmd := range u.commands {
+		cmd.execute(u.urls)
 	}
 }
 
-func (u *URLRepository) Save(url domain.URL) error {
-	if err := url.Validate(); err != nil {
-		return err
-	}
-	if _, exists := u.urls[url.ShortCode]; exists {
-		return domain.ErrDuplicateShortCode
+func (u *URLRepository) Save(url *domain.URL) error {
+	if url == nil {
+		return domain.ErrURLInvalidInput
 	}
 
-	u.urls[url.ShortCode] = url
-
-	return nil
+	result := make(chan error)
+	u.commands <- saveCommand{url: url, result: result}
+	return <-result
 }
 
 func (u *URLRepository) FindByShortCode(shortCode string) (*domain.URL, error) {
-	url, exists := u.urls[shortCode]
-	if !exists {
-		return nil, domain.ErrURLNotFound
-	}
-
-	return &url, nil
+	result := make(chan findResult)
+	u.commands <- findCommand{shortCode: shortCode, result: result}
+	res := <-result
+	return res.url, res.err
 }
 
-func (u *URLRepository) IncrementClicks(shortCode string) error {
-	url, exists := u.urls[shortCode]
-	if !exists {
-		return domain.ErrURLNotFound
-	}
-
-	url.Clicks++
-
-	return nil
+func (r *URLRepository) Close() {
+	close(r.commands)
 }
