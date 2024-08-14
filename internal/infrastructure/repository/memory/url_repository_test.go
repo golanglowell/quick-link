4a88a38d23f451c6979414d692ea4db49ac104fa
maxwellgithinji <maxwellgithinji@gmail.com>
2024-08-13 20:02:32 -0400

    fix repository tests

--- internal/infrastructure/repository/memory/url_repository_test.go
+++ internal/infrastructure/repository/memory/url_repository_test.go
@@ -17,9 +17,10 @@ func fakeURL() *domain.URL {
 		Clicks:    0,
 	}
 }
+
 func TestURLRepository_Save(t *testing.T) {
 	type args struct {
-		url domain.URL
+		url *domain.URL
 	}
 	tests := []struct {
 		name    string
@@ -27,31 +28,19 @@ func TestURLRepository_Save(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "save empty url",
+			name:    "save nil url",
 			args:    args{},
 			wantErr: true,
 		},
 		{
 			name:    "save url",
-			args:    args{*fakeURL()},
+			args:    args{fakeURL()},
 			wantErr: false,
-		},
-		{
-			name:    "duplicate short code",
-			args:    args{*fakeURL()},
-			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			u := NewURLRepository()
-
-			if tt.name == "duplicate short code" {
-				u = func() *URLRepository {
-					u.urls[fakeURL().ShortCode] = *fakeURL()
-					return u
-				}()
-			}
 			if err := u.Save(tt.args.url); (err != nil) != tt.wantErr {
 				t.Errorf("URLRepository.Save() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -98,7 +87,7 @@ func TestURLRepository_FindByShortCode(t *testing.T) {
 
 			if tt.name == "find short code" {
 				u = func() *URLRepository {
-					u.urls[fakeURL().ShortCode] = *fakeURL()
+					u.urls[fakeURL().ShortCode] = fakeURL()
 					return u
 				}()
 			}
@@ -111,59 +100,6 @@ func TestURLRepository_FindByShortCode(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("URLRepository.FindByShortCode() = %v, want %v", got, tt.want)
 			}
-		})
-	}
-}
-
-func TestURLRepository_IncrementClicks(t *testing.T) {
-	type args struct {
-		shortCode string
-	}
-	tests := []struct {
-		name      string
-		args      args
-		wantErr   bool
-		wantCount int
-	}{
-		{
-			name:      "increment empty short code",
-			args:      args{},
-			wantErr:   true,
-			wantCount: 0,
-		},
-		{
-			name: "increment non-existent short code",
-			args: args{
-				shortCode: "does not exist",
-			},
-			wantErr:   true,
-			wantCount: 0,
-		},
-		{
-			name: "increment short code",
-			args: args{
-				shortCode: fakeURL().ShortCode,
-			},
-			wantErr:   false,
-			wantCount: 1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			u := NewURLRepository()
-
-			if tt.name == "increment short code" {
-				u = func() *URLRepository {
-					u.urls[fakeURL().ShortCode] = *fakeURL()
-					return u
-				}()
-			}
-
-			if err := u.IncrementClicks(tt.args.shortCode); (err != nil) != tt.wantErr {
-				t.Errorf("URLRepository.IncrementClicks() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			//TODO: assert.Equal(t, u.urls[tt.args.shortCode].Clicks, tt.wantCount)
 		})
 	}
 }
